4769847c747d010aec4b167947901eea9a7dab0a
Sanda Golcea <sgolcea@deloitte.co.uk>
2016-03-06 16:29:33 +0000

    Scraping part of comment - gets commentId, author, title, date, comment html and plain text

--- app.js
+++ app.js
@@ -5,19 +5,52 @@ var app = express();
 
 app.get('/', function(req, res){
 	var url = 'https://news.ycombinator.com/item?id=11012044';
+	var jobs = [];
 
 	request(url, function(error, response, html){
+		res.send('Hello, please check console.log for comments.');
+
 		if (!error) {
-			res.send('Response found. Please check logs.');
 			var $ = cheerio.load(html);
 
-			var firstThing = $('.athing').first().text();
-			console.log('First comment marked with athing class: ' + firstThing);
+			// json unused , since we're not passing any threadId in the path atm
+			var json = {
+				id: 0,
+				comments: []
+			}
 
 			$('.athing').filter(function() {
 				var data = $(this);
-				eachThing = data.children().first().text();
-				console.log('DATA: ' + eachThing);
+				var comment = {
+					userId: '',
+					commentId: 0,
+					html: '',
+					plainText: '',
+					date: '',
+					title: ''
+				};
+
+				comment.userId = data.find('.comhead').find('a').first().text();
+				var commentId = data.find('.comhead').find('a').last().attr('href');
+				if (commentId) {
+					comment.commentId = commentId.substr(commentId.indexOf("=") + 1);
+				}
+				comment.html = data.find('.comment').find('span').first();
+				comment.plainText = data.find('.comment').find('span').children().first().text();
+				comment.date = data.find('.comhead').find('.age').text();
+
+				// grabbing the title from the main message:
+				// before the first paragraph <p>; and after <span class='.c00'> 
+				var myComment  = String(comment.html);
+				comment.title = myComment.substr(0, myComment.indexOf("<p>")).substr(myComment.indexOf(">") + 1);
+
+				// Just to give you a taste:
+				console.log(
+					'\n\n***Comment ' + comment.commentId
+					+ '\nby: ' + comment.userId + ' at: ' + comment.date
+					+ '\ntitle: ' + comment.title
+					+ '\ncomment: ' + comment.plainText
+					);
 			});
 		}
 	});
