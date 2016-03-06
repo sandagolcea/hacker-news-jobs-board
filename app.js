733363d4ba5702229690588dbfb9b890250f03dd
Sanda Golcea <sgolcea@deloitte.co.uk>
2016-03-06 12:39:39 +0000

    Trying out html filtering with cheerio

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require('express');
+var cheerio = require('cheerio');
+var request = require('request');
 var app = express();
 
 app.get('/', function(req, res){
-	res.send('Hello World!');
+	var url = 'https://news.ycombinator.com/item?id=11012044';
+
+	request(url, function(error, response, html){
+		if (!error) {
+			res.send('Response found. Please check logs.');
+			var $ = cheerio.load(html);
+
+			var firstThing = $('.athing').first().text();
+			console.log('First comment marked with athing class: ' + firstThing);
+
+			$('.athing').filter(function() {
+				var data = $(this);
+				eachThing = data.children().first().text();
+				console.log('DATA: ' + eachThing);
+			});
+		}
+	});
 });
 
 app.listen(3000, function() {
